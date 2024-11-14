b9392cf5bff82af71785ad5a069830d76bcd5bc0
SebastianGarb <sebastiangarboza63@hotmnai.com>
2024-11-14 08:39:15 -0500

    Primera version

---
+++ T3/Program.cs
@@ -0,0 +1,33 @@
+﻿using T3;
+
+byte opcion1;
+Estudiante e = new Estudiante();
+Cita cita = new Cita();
+Console.WriteLine("Bienvenido al registro academico");
+Console.WriteLine(" * Que desea hacer: ");
+Console.WriteLine("  1.  Crear");
+Console.WriteLine("  2.  Lista");
+Console.WriteLine("  3.  Modificar");
+Console.WriteLine("  4.  Fin");
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine("\nIngrese una opcion [1-4]");
+while (!byte.TryParse(Console.ReadLine(), out opcion1) || opcion1 > 4)
+{
+    Console.Write("Error: Ingrese opcion: ");
+}
+
+switch (opcion1)
+{
+    case 1:
+       
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        Environment.Exit(0);
+        break;
+
+}
