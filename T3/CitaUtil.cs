cbd80ecd2c734910bf7db6c10dfb4ac32e230b97
SebastianGarb <sebastiangarboza63@hotmnai.com>
2024-11-14 09:13:40 -0500

    Anadido la case CitaUtil y sus metodos

--- T3/CitaUtil.cs
+++ T3/CitaUtil.cs
@@ -8,31 +8,43 @@ namespace T3
 {
     internal class CitaUtil
     {
-        public static void CrearCita(List<Cita> citas)
+        public static Cita CrearCita(Cita[] citas)
         {
-            Console.Write("Ingrese el número de la cita: ");
-            int numero = int.Parse(Console.ReadLine());
-
-            Console.Write("Ingrese el código del estudiante: ");
-            int codigo = int.Parse(Console.ReadLine());
-
-            Console.Write("Ingrese el nombre del estudiante: ");
-            string nombre = Console.ReadLine();
-
-            Console.Write("Ingrese la universidad del estudiante: ");
-            string universidad = Console.ReadLine();
-
-            Estudiante estudiante = new Estudiante(codigo, nombre, universidad);
-
-            Console.Write("Ingrese la enfermedad: ");
-            string enfermedad = Console.ReadLine();
-
-            Console.Write("Ingrese el precio de la cita: ");
-            double precio = double.Parse(Console.ReadLine());
-
-            Cita nuevaCita = new Cita(numero, estudiante, enfermedad, precio);
+            string seguir;
             
-            Console.WriteLine("Cita creada exitosamente.");
+                Console.Write("Ingrese el número de la cita: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el código del estudiante: ");
+                int codigo = int.Parse(Console.ReadLine());
+
+                Console.Write("Ingrese el nombre del estudiante: ");
+                string nombre = Console.ReadLine();
+
+                Console.Write("Ingrese la universidad del estudiante: ");
+                string universidad = Console.ReadLine();
+
+                Estudiante estudiante = new Estudiante(codigo, nombre, universidad);
+
+                Console.Write("Ingrese la enfermedad: ");
+                string enfermedad = Console.ReadLine();
+
+                Console.Write("Ingrese el precio de la cita: ");
+                double precio = double.Parse(Console.ReadLine());
+
+                
+                return new Cita(numero, estudiante, enfermedad, precio);
+        }
+
+        public static void ListarCitas(Cita[] citas)
+        {
+            double sumaTotal = 0;
+            foreach (var cita in citas)
+            {
+                Console.WriteLine(cita.ToString());
+                sumaTotal += cita.Precio;
+            }
+            Console.WriteLine($"\nSuma total de los precios: {sumaTotal:C}");
         }
     }
 }
--- T3/Program.cs
+++ T3/Program.cs
@@ -1,32 +1,39 @@
 ﻿using T3;
 
 byte opcion1;
-List<Cita> citas = new List<Cita>();
-Console.WriteLine("Bienvenido al registro academico");
-Console.WriteLine(" * Que desea hacer: ");
-Console.WriteLine("  1.  Crear");
-Console.WriteLine("  2.  Lista");
-Console.WriteLine("  3.  Modificar Universidades");
-Console.WriteLine("  4.  Fin");
-Console.WriteLine("---------------------------------");
-
-Console.WriteLine("\nIngrese una opcion [1-4]");
-while (!byte.TryParse(Console.ReadLine(), out opcion1) || opcion1 > 4)
+string opcion2;
+Cita[] citas = new Cita[0];
+do
 {
-    Console.Write("Error: Ingrese opcion: ");
-}
+    Console.WriteLine("Bienvenido al registro academico");
+    Console.WriteLine(" * Que desea hacer: ");
+    Console.WriteLine("  1.  Crear");
+    Console.WriteLine("  2.  Lista");
+    Console.WriteLine("  3.  Modificar Universidades");
+    Console.WriteLine("  4.  Fin");
+    Console.WriteLine("---------------------------------");
 
-switch (opcion1)
-{
-    case 1:
-        CitaUtil.CrearCita(citas);
-        break;
-    case 2:
-        break;
-    case 3:
-        break;
-    case 4:
-        Environment.Exit(0);
-        break;
+    Console.WriteLine("\nIngrese una opcion [1-4]");
+    while (!byte.TryParse(Console.ReadLine(), out opcion1) || opcion1 > 4)
+    {
+        Console.Write("Error: Ingrese opcion: ");
+    }
 
-}
+    switch (opcion1)
+    {
+        case 1:
+            CitaUtil.CrearCita(citas);
+            break;
+        case 2:
+            CitaUtil.ListarCitas(citas);
+            break;
+        case 3:
+            break;
+        case 4:
+            Environment.Exit(0);
+            break;
+
+    }
+    Console.WriteLine("Quieres regresar al menu? (si/no)");
+    opcion2 = Console.ReadLine();
+} while (opcion2 == "si");
